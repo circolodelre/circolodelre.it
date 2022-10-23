785a767a8a02de2d64fd44b5e6666b28c6129ba8
francescobianco <info.francescobianco@gmail.com>
2022-10-23 18:49:43 +0200

    Merge branch 'main' of https://github.com/circolodelre/www.circolodelre.it
    # Conflicts:
    #	Makefile
    #	composer.json
    #	composer.lock
    #	src/other.php

--- src/index.php
+++ src/index.php
@@ -4,23 +4,27 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 date_default_timezone_set('Europe/Rome');
 
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+use Slim\Factory\AppFactory;
+
 require __DIR__ . '/../vendor/autoload.php';
 
-$app = new \Slim\App([
-    'settings' => [
-        'displayErrorDetails' => true,
-    ],
-]);
+$app = AppFactory::create();
 
+$app->addRoutingMiddleware();
+$errorMiddleware = $app->addErrorMiddleware(true, true, true);
 
-$app->get('/', function ($request, $response, $args) use ($settings) {
+$app->get('/', function (Request $request, Response $response, $args) {
+    $settings = [
+        'year' => 2022
+    ];
     $file = __DIR__ . '/storage/json/' .$settings['year'].'/Championship.json';
-
+var_dump($_SERVER);
     return $this->view->render($response, 'index.phtml', [
         'settings' => $settings,
         'championship' => json_decode(file_get_contents($file), true),
     ]);
 });
 
-
-
+$app->run();
