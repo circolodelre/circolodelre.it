79a6b613d4492fdb86323bd30bd221e2f8b197e2
francescobianco <info.francescobianco@gmail.com>
2022-12-30 09:54:19 +0100

    Merge branch 'main' of https://github.com/circolodelre/www.circolodelre.it
    # Conflicts:
    #	Makefile
    #	composer.json
    #	composer.lock
    #	src/other.php

--- src/pages/campionato-promozionale-2018.php
+++ src/pages/campionato-promozionale-2018.php
@@ -4,17 +4,7 @@ $twig = services::get('twig');
 $config = services::get('config');
 $year = 2018;
 
-
-if (is_dir($path)) {
-
-} else {
-
-}
-var_dump();
-
-var_dump($_SERVER);
-
-return $twig->render('index.html', [
+return $twig->render('season.html', [
     'year' => $year,
     'today' => date($config['date_format']),
     'rank' => \App\Season::loadRank($year)
--- src/router.php
+++ src/router.php
@@ -14,12 +14,11 @@ if (PHP_SAPI == 'cli-server') {
 }
 
 #var_dump($_SERVER['REQUEST_URI']);
-
-$requestUri = $_SERVER['REQUEST_URI'];
+#$requestUri = $_SERVER['REQUEST_URI'];
 
 require_once __DIR__.'/../vendor/autoload.php';
 
-$html = require_once __DIR__.'/pages/index.php';
+$path = rtrim(__DIR__.'/pages'.parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/');
+$html = require_once is_dir($path) ? $path.'/index.php' : $path.'.php';
 
 echo $html;
-
