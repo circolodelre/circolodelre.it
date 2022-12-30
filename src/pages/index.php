4b116a9204a9b22c3e1cecc99366d74223a6377a
francescobianco <info.francescobianco@gmail.com>
2022-12-30 09:30:44 +0100

    fix

--- src/pages/index.php
+++ src/pages/index.php
@@ -4,10 +4,8 @@ $twig = services::get('twig');
 $config = services::get('config');
 $year = 2018;
 
-$html = $twig->render('index.html', [
+return $twig->render('index.html', [
     'year' => $year,
     'today' => date($config['date_format']),
     'rank' => \App\Season::loadRank($year)
 ]);
-
-return $html;
