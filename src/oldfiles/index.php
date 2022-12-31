675b54baf0a66b2b6d727086c8790b9bb602ac3f
francescobianco <info.francescobianco@gmail.com>
2022-12-31 13:02:09 +0100

    Merge branch 'main' of https://github.com/circolodelre/www.circolodelre.it
    # Conflicts:
    #	Makefile
    #	composer.json
    #	composer.lock
    #	src/other.php

--- src/oldfiles/index.php
+++ src/oldfiles/index.php
@@ -3,36 +3,3 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 date_default_timezone_set('Europe/Rome');
-
-use \Psr\Http\Message\ServerRequestInterface as Request;
-use \Psr\Http\Message\ResponseInterface as Response;
-use Slim\Factory\AppFactory;
-use Slim\Views\Twig;
-use Slim\Views\TwigMiddleware;
-
-require __DIR__ . '/../vendor/autoload.php';
-
-$app = AppFactory::create();
-$twig = Twig::create('templates', ['cache' => false]);
-
-$app->addRoutingMiddleware();
-$app->add(TwigMiddleware::create($app, $twig));
-
-$errorMiddleware = $app->addErrorMiddleware(true, true, true);
-
-$app->get('/', function (Request $request, Response $response, $args) {
-    $view = Twig::fromRequest($request);
-
-    $settings = [
-        'year' => 2022
-    ];
-    $file = __DIR__ . '/storage/json/' .$settings['year'].'/Championship.json';
-#var_dump($_SERVER);
-
-    return $view->render($response, 'index.html', [
-        'settings' => $settings,
-        #'championship' => json_decode(file_get_contents($file), true),
-    ]);
-});
-
-$app->run();
--- src/oldfiles/other.php
+++ src/oldfiles/other.php
@@ -3,11 +3,6 @@
 #ini_set('display_errors', 1);
 date_default_timezone_set('Europe/Rome');
 
-use Psr\Http\Message\ResponseInterface as Response;
-use Psr\Http\Message\ServerRequestInterface as Request;
-use Slim\Factory\AppFactory;
-
-
 if (PHP_SAPI == 'cli-server') {
     $file = __DIR__ . $_SERVER['REQUEST_URI'];
     if (is_file($file)) {
@@ -24,18 +19,6 @@ if ($_SERVER['SERVER_NAME'] != 'localhost'
     exit();
 }
 
-require_once __DIR__ . '/vendor/autoload.php';
-require_once __DIR__ . '/functions.php';
-
-session_start();
-
-$settings = circolodelre_load_settings();
-#circolodelre_load_language($settings['country']);
-
-$app = AppFactory::create();
-
-$container = $app->getContainer();
-$container['view'] = new \Slim\Views\PhpRenderer('./templates');
 $container['logger'] = function () {
     $logger = new \Monolog\Logger('debug');
     $logger->pushProcessor(new \Monolog\Processor\UidProcessor());
@@ -43,22 +26,3 @@ $container['logger'] = function () {
     return $logger;
 };
 
-$app->get('/', function ($request, $response, $args) use ($settings) {
-    $file = __DIR__ . '/storage/json/' .$settings['year'].'/Championship.json';
-
-    return $this->view->render($response, 'index.phtml', [
-        'settings' => $settings,
-        'championship' => json_decode(file_get_contents($file), true),
-    ]);
-});
-
-$app->get('/'._('standing'), function ($request, $response, $args) use ($settings) {
-    $file = __DIR__ . '/storage/json/' .$settings['year'].'/Championship.json';
-
-    return $this->view->render($response, 'index.phtml', [
-        'settings' => $settings,
-        'championship' => json_decode(file_get_contents($file), true),
-    ]);
-});
-
-$app->run();
