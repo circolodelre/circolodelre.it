4200628f894c9fd1de4419ea7dd9d27bc2109308
francescobianco <info.francescobianco@gmail.com>
2022-12-31 11:30:20 +0100

    Merge branch 'main' of https://github.com/circolodelre/www.circolodelre.it
    # Conflicts:
    #	Makefile
    #	composer.json
    #	composer.lock
    #	src/other.php

--- src/tasks/rank.php
+++ src/tasks/rank.php
@@ -5,12 +5,12 @@ require_once __DIR__.'/../../vendor/autoload.php';
 use App\Season;
 use App\Standing;
 
+$config = services::get('config');
+
 $year = 2018;
 $season = Season::getSeason($year);
 
 echo "Season: {$year}\n";
-
-$config = services::get('config');
 
 $standings = Standing::loadSeason($season['csv_dir'], $config['date_format']);
 
@@ -18,10 +18,13 @@ if (empty($standings)) {
     die("File error: Standing files not found.\n");
 }
 
-$rank = Season::getRank($year, $standings);
+$rank = Season::createRank($year, $standings);
 
 is_dir($season['json_dir']) or mkdir($season['json_dir'], 0777, true);
 
-$size = file_put_contents($season['json_dir'].'/rank.json', json_encode($rank, JSON_PRETTY_PRINT));
+$size = file_put_contents(
+    $season['json_dir'].'/rank.json',
+    json_encode($rank, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES)
+);
 
 echo "Championship file updated.\n";
