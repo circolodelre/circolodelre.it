2ad9cc016d4811218bec1c638be51b95c9ed35b6
francescobianco <info.francescobianco@gmail.com>
2024-01-10 03:30:53 +0100

    fix

--- src/router.php
+++ src/router.php
@@ -5,16 +5,13 @@ ini_set('display_errors', true);
 
 if (PHP_SAPI == 'cli-server') {
     $file = __DIR__ . '/../docs' . $_SERVER['REQUEST_URI'];
-    if (is_file($file)) {
+    if (!preg_match('/.html$/i', $file) && is_file($file)) {
         $contentType = preg_match('/\.css$/i', $file) ? 'text/css' : mime_content_type($file);
         header('Content-Type: '.$contentType);
         readfile($file);
         return true;
     }
 }
-
-#var_dump($_SERVER['REQUEST_URI']);
-#$requestUri = $_SERVER['REQUEST_URI'];
 
 require_once __DIR__.'/../vendor/autoload.php';
 
