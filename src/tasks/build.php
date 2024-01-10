52db990f14125ec00ab567f5f67135c561dc4029
francesco <info.francescobianco@gmail.com>
2024-01-10 18:30:59 +0100

    fix

--- src/tasks/build.php
+++ src/tasks/build.php
@@ -6,6 +6,12 @@ require_once __DIR__.'/../../vendor/autoload.php';
 
 $config = services::get('config');
 
+echo "Download events\n";
+foreach ($config['events'] as $key => $file) {
+    $csv = file_get_contents($file);
+    file_put_contents(__DIR__.'/../events/confing-'.$key.'.csv');
+}
+
 echo "Build... \n";
 
 foreach ($config['pages'] as $page => $file) {
