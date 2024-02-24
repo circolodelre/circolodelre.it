0019cdd91a433018f75d852245ef70780bfaa569
francescobianco <info.francescobianco@gmail.com>
2024-02-24 10:54:10 +0100

    fix

--- src/pages/event.php
+++ src/pages/event.php
@@ -7,7 +7,7 @@ $config = services::get('config');
 $eventSlug = basename($_SERVER['REQUEST_URI'], '.html');
 $event = Events::loadEventBySlug($eventSlug);
 
-return $twig->render('pages/tournament-registration.html', [
+return $twig->render('event.html', [
     'year' => '2024',
     'today' => date($config['date_format']),
     'tournament' => 'campionato-provinciale-2023',
