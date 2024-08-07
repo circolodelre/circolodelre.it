3be98fb882079256e0400f811b36f92878476f69
francesco <info.francescobianco@gmail.com>
2024-08-07 18:01:36 +0200

    Fix

--- src/app/Events.php
+++ src/app/Events.php
@@ -132,7 +132,7 @@ class Events
             $eventUrl = '/'.$eventSlug.'.html';
             $flyerUrl = '/'.$eventSlug.'.pdf';
             $subscribeUrl = str_replace('{event}', urlencode($eventUniqueName), $config['subscribe']);
-            $opening = $time - (40 * 24 * 60 * 60);
+            $opening = $time - (60 * 24 * 60 * 60);
             $event = [
                 'slug' => $eventSlug,
                 'url' => $eventUrl,
