ce01d5122d8ad79fc044ea7189ec008d3f3b9c92
francesco <info.francescobianco@gmail.com>
2026-03-05 11:13:39 +0100

    refactor(twig): remove unused gettext translation function from twig service

--- src/services/twig.php
+++ src/services/twig.php
@@ -7,10 +7,6 @@ $config = Services::get('config');
 $loader = new \Twig\Loader\FilesystemLoader(__DIR__.'/../views');
 $twig = new \Twig\Environment($loader, ['cache' => false]);
 
-$twig->addFunction(new \Twig\TwigFunction('_', function ($message) {
-    return _($message);
-}));
-
 $twig->addFilter(new \Twig\TwigFilter('strftime', function ($date) use ($config) {
     return @strftime($config['date_format'], $date);
 }));
